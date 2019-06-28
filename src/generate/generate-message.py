a2049b2322aaec42e351dde410ffcbad8e62910c
Patrick José Pereira <patrickelectric@gmail.com>
2019-06-28 11:07:21 -0300

    Add ping-message-all header with global enums
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/generate/generate-message.py
+++ src/generate/generate-message.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
 from pathlib import Path
 
@@ -29,8 +30,20 @@ definitions = [ "common",
                 "ping1d",
                 "ping360"]
 
+# Create global json
+mergedJson = {"global": {}}
 for definition in definitions:
     definitionFile = "%s/%s.json" % (definitionPath, definition)
+    mergedJson["global"][definition] = json.loads(open(definitionFile, 'r').read())
+
+# Create message definitions for each sensor
+for definition in definitions:
     f = open("%s/ping-message-%s.h" % (args.output_directory, definition), "w")
-    f.write(g.generate(definitionFile, templateFile, {"definition": definition}))
+    f.write(g.generate(mergedJson["global"][definition], templateFile, {"definition": definition}))
     f.close()
+
+# Create header with global information
+templateFileAll = "%s/ping-message-all.h.in" % templatePath
+f = open("%s/ping-message-all.h" % (args.output_directory), "w")
+f.write(g.generate(mergedJson, templateFileAll))
+f.close()
