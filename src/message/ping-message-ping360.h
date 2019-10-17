0349265390b153e02e5b52bef3321960911814a6
Travis CI User <travis@example.org>
2019-10-17 15:56:16 +0000

    update autogenerated files

--- src/message/ping-message-ping360.h
+++ src/message/ping-message-ping360.h
@@ -12,11 +12,32 @@
 // TODO: should maybe be an enum
 namespace Ping360Id
 {
+    static const uint16_t DEVICE_ID = 2000;
     static const uint16_t DEVICE_DATA = 2300;
     static const uint16_t TRANSDUCER = 2601;
     static const uint16_t RESET = 2600;
-    static const uint16_t DEVICE_ID = 2000;
 }
+
+class ping360_device_id : public ping_message
+{
+public:
+    ping360_device_id(const ping_message& msg) : ping_message { msg } {}
+    ping360_device_id(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    ping360_device_id() : ping_message { static_cast<uint16_t>(12) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 2000; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint8_t id() const { return (uint8_t&)msgData[headerLength + 0]; }
+    void set_id(const uint8_t id) { (uint8_t&)msgData[headerLength + 0] = id; }
+    uint8_t reserved() const { return (uint8_t&)msgData[headerLength + 1]; }
+    void set_reserved(const uint8_t reserved) { (uint8_t&)msgData[headerLength + 1] = reserved; }
+};
 
 class ping360_device_data : public ping_message
 {
@@ -107,27 +128,6 @@ public:
 
     uint8_t bootloader() const { return (uint8_t&)msgData[headerLength + 0]; }
     void set_bootloader(const uint8_t bootloader) { (uint8_t&)msgData[headerLength + 0] = bootloader; }
-    uint8_t reserved() const { return (uint8_t&)msgData[headerLength + 1]; }
-    void set_reserved(const uint8_t reserved) { (uint8_t&)msgData[headerLength + 1] = reserved; }
-};
-
-class ping360_device_id : public ping_message
-{
-public:
-    ping360_device_id(const ping_message& msg) : ping_message { msg } {}
-    ping360_device_id(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    ping360_device_id() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 2000; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint8_t id() const { return (uint8_t&)msgData[headerLength + 0]; }
-    void set_id(const uint8_t id) { (uint8_t&)msgData[headerLength + 0] = id; }
     uint8_t reserved() const { return (uint8_t&)msgData[headerLength + 1]; }
     void set_reserved(const uint8_t reserved) { (uint8_t&)msgData[headerLength + 1] = reserved; }
 };
