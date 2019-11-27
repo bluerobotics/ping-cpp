7ac5563cda5a72bdfa3472a41505368b7db83d48
Travis CI User <travis@example.org>
2019-11-27 15:10:42 +0000

    update autogenerated files

--- src/message/ping-message-all.h
+++ src/message/ping-message-all.h
@@ -7,10 +7,10 @@
 
 #pragma once
 
+#include "ping-message-ping360.h"
+#include "ping-message-common.h"
 #include "ping-message-ping1d.h"
 #include "ping-message-bluebps.h"
-#include "ping-message-common.h"
-#include "ping-message-ping360.h"
 
 // Declare an empty Q_NAMESPACE and Q_ENUM_NS if it's not a Qt project, otherwise include QObject
 #ifndef QT_CORE_LIB
@@ -30,62 +30,62 @@ enum class PingDeviceType {
 Q_ENUM_NS(PingDeviceType)
 
 enum class PingMessageId {
-    PING1D_SET_PING_INTERVAL = 1004,
-    PING1D_SET_SPEED_OF_SOUND = 1002,
-    PING1D_SET_DEVICE_ID = 1000,
-    PING1D_SET_GAIN_SETTING = 1005,
-    PING1D_SET_PING_ENABLE = 1006,
-    PING1D_SET_RANGE = 1001,
-    PING1D_SET_MODE_AUTO = 1003,
-    PING1D_PROFILE = 1300,
-    PING1D_VOLTAGE_5 = 1202,
-    PING1D_TRANSMIT_DURATION = 1208,
-    PING1D_PROCESSOR_TEMPERATURE = 1213,
-    PING1D_GAIN_SETTING = 1207,
-    PING1D_PCB_TEMPERATURE = 1214,
-    PING1D_GENERAL_INFO = 1210,
-    PING1D_MODE_AUTO = 1205,
-    PING1D_PING_ENABLE = 1215,
-    PING1D_PING_INTERVAL = 1206,
-    PING1D_FIRMWARE_VERSION = 1200,
-    PING1D_DISTANCE_SIMPLE = 1211,
-    PING1D_RANGE = 1204,
-    PING1D_SPEED_OF_SOUND = 1203,
-    PING1D_DEVICE_ID = 1201,
-    PING1D_DISTANCE = 1212,
-    PING1D_CONTINUOUS_START = 1400,
-    PING1D_GOTO_BOOTLOADER = 1100,
-    PING1D_CONTINUOUS_STOP = 1401,
-    BLUEBPS_LPF_SETTING = 9007,
-    BLUEBPS_CURRENT_MAX = 9002,
-    BLUEBPS_CELL_VOLTAGE_MINIMUM = 9000,
-    BLUEBPS_CURRENT_TIMEOUT = 9003,
-    BLUEBPS_LPF_SAMPLE_FREQUENCY = 9007,
-    BLUEBPS_TEMPERATURE_TIMEOUT = 9005,
-    BLUEBPS_CELL_VOLTAGE_TIMEOUT = 9001,
-    BLUEBPS_TEMPERATURE_MAX = 9004,
-    BLUEBPS_STREAM_RATE = 9006,
-    BLUEBPS_CURRENT_MAX = 9102,
-    BLUEBPS_CURRENT_TIMEOUT = 9103,
-    BLUEBPS_EVENTS = 9107,
-    BLUEBPS_CELL_VOLTAGE_MIN = 9100,
-    BLUEBPS_TEMPERATURE_TIMEOUT = 9105,
-    BLUEBPS_TEMPERATURE_MAX = 9104,
-    BLUEBPS_CELL_TIMEOUT = 9101,
-    BLUEBPS_STATE = 9106,
-    BLUEBPS_RESET_DEFAULTS = 9202,
-    BLUEBPS_ERASE_FLASH = 9201,
-    BLUEBPS_REBOOT = 9200,
-    COMMON_ASCII_TEXT = 3,
-    COMMON_GENERAL_REQUEST = 6,
-    COMMON_NACK = 2,
-    COMMON_ACK = 1,
-    COMMON_PROTOCOL_VERSION = 5,
-    COMMON_DEVICE_INFORMATION = 4,
     PING360_DEVICE_ID = 2000,
     PING360_DEVICE_DATA = 2300,
     PING360_TRANSDUCER = 2601,
     PING360_RESET = 2600,
+    COMMON_DEVICE_INFORMATION = 4,
+    COMMON_PROTOCOL_VERSION = 5,
+    COMMON_NACK = 2,
+    COMMON_ACK = 1,
+    COMMON_GENERAL_REQUEST = 6,
+    COMMON_ASCII_TEXT = 3,
+    PING1D_SET_RANGE = 1001,
+    PING1D_SET_MODE_AUTO = 1003,
+    PING1D_SET_SPEED_OF_SOUND = 1002,
+    PING1D_SET_PING_INTERVAL = 1004,
+    PING1D_SET_DEVICE_ID = 1000,
+    PING1D_SET_PING_ENABLE = 1006,
+    PING1D_SET_GAIN_SETTING = 1005,
+    PING1D_MODE_AUTO = 1205,
+    PING1D_PROCESSOR_TEMPERATURE = 1213,
+    PING1D_PING_INTERVAL = 1206,
+    PING1D_PCB_TEMPERATURE = 1214,
+    PING1D_PROFILE = 1300,
+    PING1D_RANGE = 1204,
+    PING1D_DISTANCE = 1212,
+    PING1D_VOLTAGE_5 = 1202,
+    PING1D_DISTANCE_SIMPLE = 1211,
+    PING1D_GENERAL_INFO = 1210,
+    PING1D_FIRMWARE_VERSION = 1200,
+    PING1D_TRANSMIT_DURATION = 1208,
+    PING1D_PING_ENABLE = 1215,
+    PING1D_GAIN_SETTING = 1207,
+    PING1D_DEVICE_ID = 1201,
+    PING1D_SPEED_OF_SOUND = 1203,
+    PING1D_CONTINUOUS_STOP = 1401,
+    PING1D_GOTO_BOOTLOADER = 1100,
+    PING1D_CONTINUOUS_START = 1400,
+    BLUEBPS_TEMPERATURE_TIMEOUT = 9005,
+    BLUEBPS_CURRENT_MAX = 9002,
+    BLUEBPS_CURRENT_TIMEOUT = 9003,
+    BLUEBPS_LPF_SETTING = 9007,
+    BLUEBPS_STREAM_RATE = 9006,
+    BLUEBPS_LPF_SAMPLE_FREQUENCY = 9007,
+    BLUEBPS_CELL_VOLTAGE_TIMEOUT = 9001,
+    BLUEBPS_CELL_VOLTAGE_MINIMUM = 9000,
+    BLUEBPS_TEMPERATURE_MAX = 9004,
+    BLUEBPS_CELL_VOLTAGE_MIN = 9100,
+    BLUEBPS_CURRENT_MAX = 9102,
+    BLUEBPS_CURRENT_TIMEOUT = 9103,
+    BLUEBPS_TEMPERATURE_TIMEOUT = 9105,
+    BLUEBPS_EVENTS = 9107,
+    BLUEBPS_STATE = 9106,
+    BLUEBPS_CELL_TIMEOUT = 9101,
+    BLUEBPS_TEMPERATURE_MAX = 9104,
+    BLUEBPS_REBOOT = 9200,
+    BLUEBPS_RESET_DEFAULTS = 9202,
+    BLUEBPS_ERASE_FLASH = 9201,
 };
 Q_ENUM_NS(PingMessageId)
 }
@@ -115,6 +115,131 @@ public:
                 return "Ping360";
             default:
                 return "Unregistered";
+        }
+    }
+
+    /**
+     * @brief Helper function that provides the name of a message from the message ID number
+     *
+     * @param messageId
+     * @return constexpr const char*
+     */
+    static constexpr const char* nameFromMessageId(const PingEnumNamespace::PingMessageId messageId) {
+        switch(messageId) {
+            case(PingEnumNamespace::PingMessageId::PING360_DEVICE_ID):
+                return "DEVICE_ID";
+            case(PingEnumNamespace::PingMessageId::PING360_DEVICE_DATA):
+                return "DEVICE_DATA";
+            case(PingEnumNamespace::PingMessageId::PING360_TRANSDUCER):
+                return "TRANSDUCER";
+            case(PingEnumNamespace::PingMessageId::PING360_RESET):
+                return "RESET";
+            case(PingEnumNamespace::PingMessageId::COMMON_DEVICE_INFORMATION):
+                return "DEVICE_INFORMATION";
+            case(PingEnumNamespace::PingMessageId::COMMON_PROTOCOL_VERSION):
+                return "PROTOCOL_VERSION";
+            case(PingEnumNamespace::PingMessageId::COMMON_NACK):
+                return "NACK";
+            case(PingEnumNamespace::PingMessageId::COMMON_ACK):
+                return "ACK";
+            case(PingEnumNamespace::PingMessageId::COMMON_GENERAL_REQUEST):
+                return "GENERAL_REQUEST";
+            case(PingEnumNamespace::PingMessageId::COMMON_ASCII_TEXT):
+                return "ASCII_TEXT";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_RANGE):
+                return "SET_RANGE";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_MODE_AUTO):
+                return "SET_MODE_AUTO";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_SPEED_OF_SOUND):
+                return "SET_SPEED_OF_SOUND";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_PING_INTERVAL):
+                return "SET_PING_INTERVAL";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_DEVICE_ID):
+                return "SET_DEVICE_ID";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_PING_ENABLE):
+                return "SET_PING_ENABLE";
+            case(PingEnumNamespace::PingMessageId::PING1D_SET_GAIN_SETTING):
+                return "SET_GAIN_SETTING";
+            case(PingEnumNamespace::PingMessageId::PING1D_MODE_AUTO):
+                return "MODE_AUTO";
+            case(PingEnumNamespace::PingMessageId::PING1D_PROCESSOR_TEMPERATURE):
+                return "PROCESSOR_TEMPERATURE";
+            case(PingEnumNamespace::PingMessageId::PING1D_PING_INTERVAL):
+                return "PING_INTERVAL";
+            case(PingEnumNamespace::PingMessageId::PING1D_PCB_TEMPERATURE):
+                return "PCB_TEMPERATURE";
+            case(PingEnumNamespace::PingMessageId::PING1D_PROFILE):
+                return "PROFILE";
+            case(PingEnumNamespace::PingMessageId::PING1D_RANGE):
+                return "RANGE";
+            case(PingEnumNamespace::PingMessageId::PING1D_DISTANCE):
+                return "DISTANCE";
+            case(PingEnumNamespace::PingMessageId::PING1D_VOLTAGE_5):
+                return "VOLTAGE_5";
+            case(PingEnumNamespace::PingMessageId::PING1D_DISTANCE_SIMPLE):
+                return "DISTANCE_SIMPLE";
+            case(PingEnumNamespace::PingMessageId::PING1D_GENERAL_INFO):
+                return "GENERAL_INFO";
+            case(PingEnumNamespace::PingMessageId::PING1D_FIRMWARE_VERSION):
+                return "FIRMWARE_VERSION";
+            case(PingEnumNamespace::PingMessageId::PING1D_TRANSMIT_DURATION):
+                return "TRANSMIT_DURATION";
+            case(PingEnumNamespace::PingMessageId::PING1D_PING_ENABLE):
+                return "PING_ENABLE";
+            case(PingEnumNamespace::PingMessageId::PING1D_GAIN_SETTING):
+                return "GAIN_SETTING";
+            case(PingEnumNamespace::PingMessageId::PING1D_DEVICE_ID):
+                return "DEVICE_ID";
+            case(PingEnumNamespace::PingMessageId::PING1D_SPEED_OF_SOUND):
+                return "SPEED_OF_SOUND";
+            case(PingEnumNamespace::PingMessageId::PING1D_CONTINUOUS_STOP):
+                return "CONTINUOUS_STOP";
+            case(PingEnumNamespace::PingMessageId::PING1D_GOTO_BOOTLOADER):
+                return "GOTO_BOOTLOADER";
+            case(PingEnumNamespace::PingMessageId::PING1D_CONTINUOUS_START):
+                return "CONTINUOUS_START";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_TEMPERATURE_TIMEOUT):
+                return "TEMPERATURE_TIMEOUT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CURRENT_MAX):
+                return "CURRENT_MAX";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CURRENT_TIMEOUT):
+                return "CURRENT_TIMEOUT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_LPF_SETTING):
+                return "LPF_SETTING";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_STREAM_RATE):
+                return "STREAM_RATE";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_LPF_SAMPLE_FREQUENCY):
+                return "LPF_SAMPLE_FREQUENCY";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CELL_VOLTAGE_TIMEOUT):
+                return "CELL_VOLTAGE_TIMEOUT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CELL_VOLTAGE_MINIMUM):
+                return "CELL_VOLTAGE_MINIMUM";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_TEMPERATURE_MAX):
+                return "TEMPERATURE_MAX";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CELL_VOLTAGE_MIN):
+                return "CELL_VOLTAGE_MIN";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CURRENT_MAX):
+                return "CURRENT_MAX";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CURRENT_TIMEOUT):
+                return "CURRENT_TIMEOUT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_TEMPERATURE_TIMEOUT):
+                return "TEMPERATURE_TIMEOUT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_EVENTS):
+                return "EVENTS";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_STATE):
+                return "STATE";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_CELL_TIMEOUT):
+                return "CELL_TIMEOUT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_TEMPERATURE_MAX):
+                return "TEMPERATURE_MAX";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_REBOOT):
+                return "REBOOT";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_RESET_DEFAULTS):
+                return "RESET_DEFAULTS";
+            case(PingEnumNamespace::PingMessageId::BLUEBPS_ERASE_FLASH):
+                return "ERASE_FLASH";
+            default:
+                return "UNKNOWN";
         }
     }
 
--- src/message/ping-message-bluebps.h
+++ src/message/ping-message-bluebps.h
@@ -12,45 +12,45 @@
 // TODO: should maybe be an enum
 namespace BluebpsId
 {
-    static const uint16_t LPF_SETTING = 9007;
-    static const uint16_t CURRENT_MAX = 9002;
-    static const uint16_t CELL_VOLTAGE_MINIMUM = 9000;
-    static const uint16_t CURRENT_TIMEOUT = 9003;
-    static const uint16_t LPF_SAMPLE_FREQUENCY = 9007;
     static const uint16_t TEMPERATURE_TIMEOUT = 9005;
-    static const uint16_t CELL_VOLTAGE_TIMEOUT = 9001;
-    static const uint16_t TEMPERATURE_MAX = 9004;
+    static const uint16_t CURRENT_MAX = 9002;
+    static const uint16_t CURRENT_TIMEOUT = 9003;
+    static const uint16_t LPF_SETTING = 9007;
     static const uint16_t STREAM_RATE = 9006;
+    static const uint16_t LPF_SAMPLE_FREQUENCY = 9007;
+    static const uint16_t CELL_VOLTAGE_TIMEOUT = 9001;
+    static const uint16_t CELL_VOLTAGE_MINIMUM = 9000;
+    static const uint16_t TEMPERATURE_MAX = 9004;
+    static const uint16_t CELL_VOLTAGE_MIN = 9100;
     static const uint16_t CURRENT_MAX = 9102;
     static const uint16_t CURRENT_TIMEOUT = 9103;
-    static const uint16_t EVENTS = 9107;
-    static const uint16_t CELL_VOLTAGE_MIN = 9100;
     static const uint16_t TEMPERATURE_TIMEOUT = 9105;
-    static const uint16_t TEMPERATURE_MAX = 9104;
-    static const uint16_t CELL_TIMEOUT = 9101;
+    static const uint16_t EVENTS = 9107;
     static const uint16_t STATE = 9106;
+    static const uint16_t CELL_TIMEOUT = 9101;
+    static const uint16_t TEMPERATURE_MAX = 9104;
+    static const uint16_t REBOOT = 9200;
     static const uint16_t RESET_DEFAULTS = 9202;
     static const uint16_t ERASE_FLASH = 9201;
-    static const uint16_t REBOOT = 9200;
 }
 
-class bluebps_lpf_setting : public ping_message
+class bluebps_temperature_timeout : public ping_message
 {
 public:
-    bluebps_lpf_setting(const ping_message& msg) : ping_message { msg } {}
-    bluebps_lpf_setting(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_lpf_setting() : ping_message { static_cast<uint16_t>(12) }
+    bluebps_temperature_timeout(const ping_message& msg) : ping_message { msg } {}
+    bluebps_temperature_timeout(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_temperature_timeout() : ping_message { static_cast<uint16_t>(12) }
     {
         msgData[0] = 'B';
         msgData[1] = 'R';
         (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9007; // ID
+        (uint16_t&)msgData[4] = 9005; // ID
         msgData[6] = 0;
         msgData[7] = 0;
     }
 
-    uint16_t setting() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_setting(const uint16_t setting) { (uint16_t&)msgData[headerLength + 0] = setting; }
+    uint16_t timeout() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
 };
 
 class bluebps_current_max : public ping_message
@@ -64,25 +64,6 @@ public:
         msgData[1] = 'R';
         (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
         (uint16_t&)msgData[4] = 9002; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint16_t limit() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
-};
-
-class bluebps_cell_voltage_minimum : public ping_message
-{
-public:
-    bluebps_cell_voltage_minimum(const ping_message& msg) : ping_message { msg } {}
-    bluebps_cell_voltage_minimum(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_cell_voltage_minimum() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9000; // ID
         msgData[6] = 0;
         msgData[7] = 0;
     }
@@ -110,6 +91,44 @@ public:
     void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
 };
 
+class bluebps_lpf_setting : public ping_message
+{
+public:
+    bluebps_lpf_setting(const ping_message& msg) : ping_message { msg } {}
+    bluebps_lpf_setting(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_lpf_setting() : ping_message { static_cast<uint16_t>(12) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 9007; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint16_t setting() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_setting(const uint16_t setting) { (uint16_t&)msgData[headerLength + 0] = setting; }
+};
+
+class bluebps_stream_rate : public ping_message
+{
+public:
+    bluebps_stream_rate(const ping_message& msg) : ping_message { msg } {}
+    bluebps_stream_rate(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_stream_rate() : ping_message { static_cast<uint16_t>(14) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(4); // payload size
+        (uint16_t&)msgData[4] = 9006; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint32_t rate() const { return (uint32_t&)msgData[headerLength + 0]; }
+    void set_rate(const uint32_t rate) { (uint32_t&)msgData[headerLength + 0] = rate; }
+};
+
 class bluebps_lpf_sample_frequency : public ping_message
 {
 public:
@@ -127,25 +146,6 @@ public:
 
     uint32_t sample_frequency() const { return (uint32_t&)msgData[headerLength + 0]; }
     void set_sample_frequency(const uint32_t sample_frequency) { (uint32_t&)msgData[headerLength + 0] = sample_frequency; }
-};
-
-class bluebps_temperature_timeout : public ping_message
-{
-public:
-    bluebps_temperature_timeout(const ping_message& msg) : ping_message { msg } {}
-    bluebps_temperature_timeout(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_temperature_timeout() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9005; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint16_t timeout() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
 };
 
 class bluebps_cell_voltage_timeout : public ping_message
@@ -167,6 +167,25 @@ public:
     void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
 };
 
+class bluebps_cell_voltage_minimum : public ping_message
+{
+public:
+    bluebps_cell_voltage_minimum(const ping_message& msg) : ping_message { msg } {}
+    bluebps_cell_voltage_minimum(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_cell_voltage_minimum() : ping_message { static_cast<uint16_t>(12) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 9000; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint16_t limit() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
+};
+
 class bluebps_temperature_max : public ping_message
 {
 public:
@@ -186,23 +205,23 @@ public:
     void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
 };
 
-class bluebps_stream_rate : public ping_message
+class bluebps_cell_voltage_min : public ping_message
 {
 public:
-    bluebps_stream_rate(const ping_message& msg) : ping_message { msg } {}
-    bluebps_stream_rate(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_stream_rate() : ping_message { static_cast<uint16_t>(14) }
+    bluebps_cell_voltage_min(const ping_message& msg) : ping_message { msg } {}
+    bluebps_cell_voltage_min(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_cell_voltage_min() : ping_message { static_cast<uint16_t>(12) }
     {
         msgData[0] = 'B';
         msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(4); // payload size
-        (uint16_t&)msgData[4] = 9006; // ID
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 9100; // ID
         msgData[6] = 0;
         msgData[7] = 0;
     }
 
-    uint32_t rate() const { return (uint32_t&)msgData[headerLength + 0]; }
-    void set_rate(const uint32_t rate) { (uint32_t&)msgData[headerLength + 0] = rate; }
+    uint16_t limit() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
 };
 
 class bluebps_current_max : public ping_message
@@ -243,6 +262,25 @@ public:
     void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
 };
 
+class bluebps_temperature_timeout : public ping_message
+{
+public:
+    bluebps_temperature_timeout(const ping_message& msg) : ping_message { msg } {}
+    bluebps_temperature_timeout(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_temperature_timeout() : ping_message { static_cast<uint16_t>(12) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 9105; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint16_t timeout() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
+};
+
 class bluebps_events : public ping_message
 {
 public:
@@ -264,82 +302,6 @@ public:
     void set_current(const uint16_t current) { (uint16_t&)msgData[headerLength + 2] = current; }
     uint16_t temperature() const { return (uint16_t&)msgData[headerLength + 4]; }
     void set_temperature(const uint16_t temperature) { (uint16_t&)msgData[headerLength + 4] = temperature; }
-};
-
-class bluebps_cell_voltage_min : public ping_message
-{
-public:
-    bluebps_cell_voltage_min(const ping_message& msg) : ping_message { msg } {}
-    bluebps_cell_voltage_min(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_cell_voltage_min() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9100; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint16_t limit() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
-};
-
-class bluebps_temperature_timeout : public ping_message
-{
-public:
-    bluebps_temperature_timeout(const ping_message& msg) : ping_message { msg } {}
-    bluebps_temperature_timeout(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_temperature_timeout() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9105; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint16_t timeout() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
-};
-
-class bluebps_temperature_max : public ping_message
-{
-public:
-    bluebps_temperature_max(const ping_message& msg) : ping_message { msg } {}
-    bluebps_temperature_max(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_temperature_max() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9104; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint16_t limit() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
-};
-
-class bluebps_cell_timeout : public ping_message
-{
-public:
-    bluebps_cell_timeout(const ping_message& msg) : ping_message { msg } {}
-    bluebps_cell_timeout(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_cell_timeout() : ping_message { static_cast<uint16_t>(12) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
-        (uint16_t&)msgData[4] = 9101; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint16_t timeout() const { return (uint16_t&)msgData[headerLength + 0]; }
-    void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
 };
 
 class bluebps_state : public ping_message
@@ -375,6 +337,63 @@ public:
     void set_cell_voltages_at(const uint16_t i, const uint16_t data) { (uint16_t&)msgData[headerLength + 10 + i] = data; }
 };
 
+class bluebps_cell_timeout : public ping_message
+{
+public:
+    bluebps_cell_timeout(const ping_message& msg) : ping_message { msg } {}
+    bluebps_cell_timeout(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_cell_timeout() : ping_message { static_cast<uint16_t>(12) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 9101; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint16_t timeout() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_timeout(const uint16_t timeout) { (uint16_t&)msgData[headerLength + 0] = timeout; }
+};
+
+class bluebps_temperature_max : public ping_message
+{
+public:
+    bluebps_temperature_max(const ping_message& msg) : ping_message { msg } {}
+    bluebps_temperature_max(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_temperature_max() : ping_message { static_cast<uint16_t>(12) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(2); // payload size
+        (uint16_t&)msgData[4] = 9104; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint16_t limit() const { return (uint16_t&)msgData[headerLength + 0]; }
+    void set_limit(const uint16_t limit) { (uint16_t&)msgData[headerLength + 0] = limit; }
+};
+
+class bluebps_reboot : public ping_message
+{
+public:
+    bluebps_reboot(const ping_message& msg) : ping_message { msg } {}
+    bluebps_reboot(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
+    bluebps_reboot() : ping_message { static_cast<uint16_t>(11) }
+    {
+        msgData[0] = 'B';
+        msgData[1] = 'R';
+        (uint16_t&)msgData[2] = static_cast<uint16_t>(1); // payload size
+        (uint16_t&)msgData[4] = 9200; // ID
+        msgData[6] = 0;
+        msgData[7] = 0;
+    }
+
+    uint8_t goto_bootloader() const { return (uint8_t&)msgData[headerLength + 0]; }
+    void set_goto_bootloader(const uint8_t goto_bootloader) { (uint8_t&)msgData[headerLength + 0] = goto_bootloader; }
+};
+
 class bluebps_reset_defaults : public ping_message
 {
 public:
@@ -407,24 +426,5 @@ public:
         msgData[7] = 0;
     }
 
-};
-
-class bluebps_reboot : public ping_message
-{
-public:
-    bluebps_reboot(const ping_message& msg) : ping_message { msg } {}
-    bluebps_reboot(const uint8_t* buf, const uint16_t length) : ping_message { buf, length } {}
-    bluebps_reboot() : ping_message { static_cast<uint16_t>(11) }
-    {
-        msgData[0] = 'B';
-        msgData[1] = 'R';
-        (uint16_t&)msgData[2] = static_cast<uint16_t>(1); // payload size
-        (uint16_t&)msgData[4] = 9200; // ID
-        msgData[6] = 0;
-        msgData[7] = 0;
-    }
-
-    uint8_t goto_bootloader() const { return (uint8_t&)msgData[headerLength + 0]; }
-    void set_goto_bootloader(const uint8_t goto_bootloader) { (uint8_t&)msgData[headerLength + 0] = goto_bootloader; }
 };
 
