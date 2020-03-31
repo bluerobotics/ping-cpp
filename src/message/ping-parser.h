c01acc8c5c1740f27555c2719917a64408ccee54
Patrick José Pereira <patrickelectric@gmail.com>
2020-03-31 13:35:31 -0300

    ping-parser: Avoid possible problems with anonymous enum
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/message/ping-parser.h
+++ src/message/ping-parser.h
@@ -14,7 +14,7 @@ public:
     uint32_t errors = 0; // number of parse errors
 
     // This enum MUST be contiguous
-    enum ParseState {
+    enum class ParseState : uint8_t {
         NEW_MESSAGE,   // Just got a complete checksum-verified message
         WAIT_START,    // Waiting for the first character of a message 'B'
         WAIT_HEADER,   // Waiting for the second character in the two-character sequence 'BR'
@@ -36,25 +36,25 @@ private:
     uint32_t rxBufferLength_;
     uint32_t rxCount_ = 0;
     uint16_t payloadLength_ = 0;
-    uint8_t state_ = WAIT_START;
+    ParseState state_ = ParseState::WAIT_START;
 
 public:
     void reset()
     {
-        state_ = WAIT_START;
+        state_ = ParseState::WAIT_START;
     }
 
     ParseState parseByte(const uint8_t b)
     {
         switch(state_) {
-        case WAIT_START:
+        case ParseState::WAIT_START:
             rxCount_ = 0;
             if (b == 'B') {
                 rxBuffer_[rxCount_++] = b;
                 state_++;
             }
             break;
-        case WAIT_HEADER:
+        case ParseState::WAIT_HEADER:
             if (b == 'R') {
                 rxBuffer_[rxCount_++] = b;
                 state_++;
@@ -62,12 +62,12 @@ public:
                 state_ = WAIT_START;
             }
             break;
-        case WAIT_LENGTH_L:
+        case ParseState::WAIT_LENGTH_L:
             rxBuffer_[rxCount_++] = b;
             payloadLength_ = b;
             state_++;
             break;
-        case WAIT_LENGTH_H:
+        case ParseState::WAIT_LENGTH_H:
             rxBuffer_[rxCount_++] = b;
             payloadLength_ = static_cast<uint16_t>((b << 8) | payloadLength_);
             if (payloadLength_ <= rxBufferLength_ - 8 - 2) {
@@ -76,10 +76,10 @@ public:
                 state_ = WAIT_START;
             }
             break;
-        case WAIT_MSG_ID_L: // fall-through
-        case WAIT_MSG_ID_H:
-        case WAIT_SRC_ID:
-        case WAIT_DST_ID:
+        case ParseState::WAIT_MSG_ID_L: // fall-through
+        case ParseState::WAIT_MSG_ID_H:
+        case ParseState::WAIT_SRC_ID:
+        case ParseState::WAIT_DST_ID:
             rxBuffer_[rxCount_++] = b;
             state_++;
             if (payloadLength_ == 0) {
@@ -87,27 +87,27 @@ public:
                 state_++;
             }
             break;
-        case WAIT_PAYLOAD:
+        case ParseState::WAIT_PAYLOAD:
             rxBuffer_[rxCount_++] = b;
             if (--payloadLength_ == 0) {
                 state_++;
             }
             break;
-        case WAIT_CHECKSUM_L:
+        case ParseState::WAIT_CHECKSUM_L:
             rxBuffer_[rxCount_++] = b;
             state_++;
             break;
-        case WAIT_CHECKSUM_H:
+        case ParseState::WAIT_CHECKSUM_H:
             rxBuffer_[rxCount_++] = b;
-            state_ = WAIT_START;
+            state_ = ParseState::WAIT_START;
             if (rxMessage.verifyChecksum()) {
                 parsed++;
-                return NEW_MESSAGE;
+                return ParseState::NEW_MESSAGE;
             } else {
                 errors++;
-                return ERROR;
+                return ParseState::ERROR;
             }
         }
-        return (ParseState)state_;
+        return state_;
     }
 };
