029a3e43cf13556b71981b83860cfa58c35a94ac
Patrick José Pereira <patrickelectric@gmail.com>
2020-03-31 13:38:43 -0300

    ping-parser: Organize private and public variables
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/message/ping-parser.h
+++ src/message/ping-parser.h
@@ -28,7 +28,7 @@ public:
     uint32_t errors = 0; // number of parse errors
 
     // This enum MUST be contiguous
-    enum class ParseState : uint8_t {
+    enum class State : uint8_t {
         NEW_MESSAGE,   // Just got a complete checksum-verified message
         WAIT_START,    // Waiting for the first character of a message 'B'
         WAIT_HEADER,   // Waiting for the second character in the two-character sequence 'BR'
@@ -44,94 +44,93 @@ public:
         ERROR, // Checksum didn't check out
     };
 
-    // variables used for parsing
-private:
-    uint8_t* rxBuffer_;
-    uint32_t rxBufferLength_;
-    uint32_t rxCount_ = 0;
-    uint16_t payloadLength_ = 0;
-    ParseState state_ = ParseState::WAIT_START;
-
-public:
     /**
      * @brief Reset parser state
      *
      */
     void reset()
     {
-        state_ = ParseState::WAIT_START;
+        state_ = State::WAIT_START;
     }
 
     /**
      * @brief Parse a single byte
      *
-     * @param b
-     * @return ParseState
+     * @param data
+     * @return State
      */
-    ParseState parseByte(const uint8_t b)
+    PingParser::State parseByte(const uint8_t data)
     {
         switch(state_) {
-        case ParseState::WAIT_START:
+        case PingParser::State::WAIT_START:
             rxCount_ = 0;
-            if (b == 'B') {
-                rxBuffer_[rxCount_++] = b;
+            if (data == 'B') {
+                rxBuffer_[rxCount_++] = data;
                 state_++;
             }
             break;
-        case ParseState::WAIT_HEADER:
-            if (b == 'R') {
-                rxBuffer_[rxCount_++] = b;
+        case PingParser::State::WAIT_HEADER:
+            if (data == 'R') {
+                rxBuffer_[rxCount_++] = data;
                 state_++;
             } else {
-                state_ = WAIT_START;
+                reset();
             }
             break;
-        case ParseState::WAIT_LENGTH_L:
-            rxBuffer_[rxCount_++] = b;
-            payloadLength_ = b;
+        case PingParser::State::WAIT_LENGTH_L:
+            rxBuffer_[rxCount_++] = data;
+            payloadLength_ = data;
             state_++;
             break;
-        case ParseState::WAIT_LENGTH_H:
-            rxBuffer_[rxCount_++] = b;
-            payloadLength_ = static_cast<uint16_t>((b << 8) | payloadLength_);
+        case PingParser::State::WAIT_LENGTH_H:
+            rxBuffer_[rxCount_++] = data;
+            payloadLength_ = static_cast<uint16_t>((data << 8) | payloadLength_);
             if (payloadLength_ <= rxBufferLength_ - 8 - 2) {
                 state_++;
             } else {
-                state_ = WAIT_START;
+                reset();
             }
             break;
-        case ParseState::WAIT_MSG_ID_L: // fall-through
-        case ParseState::WAIT_MSG_ID_H:
-        case ParseState::WAIT_SRC_ID:
-        case ParseState::WAIT_DST_ID:
-            rxBuffer_[rxCount_++] = b;
+        case PingParser::State::WAIT_MSG_ID_L: // fall-through
+        case PingParser::State::WAIT_MSG_ID_H:
+        case PingParser::State::WAIT_SRC_ID:
+        case PingParser::State::WAIT_DST_ID:
+            rxBuffer_[rxCount_++] = data;
             state_++;
             if (payloadLength_ == 0) {
                 // no payload bytes, so we skip WAIT_PAYLOAD state
                 state_++;
             }
             break;
-        case ParseState::WAIT_PAYLOAD:
-            rxBuffer_[rxCount_++] = b;
+        case PingParser::State::WAIT_PAYLOAD:
+            rxBuffer_[rxCount_++] = data;
             if (--payloadLength_ == 0) {
                 state_++;
             }
             break;
-        case ParseState::WAIT_CHECKSUM_L:
-            rxBuffer_[rxCount_++] = b;
+        case PingParser::State::WAIT_CHECKSUM_L:
+            rxBuffer_[rxCount_++] = data;
             state_++;
             break;
-        case ParseState::WAIT_CHECKSUM_H:
-            rxBuffer_[rxCount_++] = b;
-            state_ = ParseState::WAIT_START;
+        case PingParser::State::WAIT_CHECKSUM_H:
+            rxBuffer_[rxCount_++] = data;
+            state_ = PingParser::State::WAIT_START;
             if (rxMessage.verifyChecksum()) {
                 parsed++;
-                return ParseState::NEW_MESSAGE;
+                return PingParser::State::NEW_MESSAGE;
             } else {
                 errors++;
-                return ParseState::ERROR;
+                return PingParser::State::ERROR;
             }
         }
         return state_;
     }
+
+// variables used for parsing
+private:
+    uint8_t* rxBuffer_;
+    uint32_t rxBufferLength_;
+    uint32_t rxCount_ = 0;
+    uint16_t payloadLength_ = 0;
+    State state_ = PingParser::State::WAIT_START;
 };
