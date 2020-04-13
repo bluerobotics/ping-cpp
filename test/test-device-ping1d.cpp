b2f6324350acc31eb9caa9e891f00770a5ae1f0c
Patrick José Pereira <patrickelectric@gmail.com>
2020-04-13 14:54:09 -0300

    test-device-ping1d: test ping1d use new helper and device
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- test/test-device-ping1d.cpp
+++ test/test-device-ping1d.cpp
@@ -4,6 +4,8 @@
 
 #include "command-line/command-line.h"
 
+#include "helper.h"
+
 #include <cstdio>
 
 /**
@@ -18,96 +20,55 @@ int main(int argc, char* argv[])
     auto port = AbstractLink::openUrl(CommandLine::self()->connectionString);
     Ping1d device = Ping1d(*port.get());
 
-    printf("initializing\n");
+    // Basic information
+    PingDeviceTest::test("Device initialization", device.initialize(100));
+    PingDeviceTest::test("Device type", static_cast<PingDeviceType>(device.device_information.device_type) == PingDeviceType::PING1D);
+    PingDeviceTest::test("Device ID", device.device_id == 1);
 
-    if (!device.initialize(100)) {
-        printf("device initialization failed\n");
-    } else {
-        printf("pass\n");
-        printf("Device Type %d id %d hardware revision %d\n", device.device_type, device.device_id,
-            device.device_revision);
-        printf("Ping Protocol v%d.%d.%d\n", device.version_major, device.version_minor, device.version_patch);
-        printf("Device Firmware v%d.%d.%d\n", device.firmware_version_major, device.firmware_version_minor,
-            device.firmware_version_patch);
-    }
+    PingDeviceTest::test("Ping protocol version",
+        device.protocol_version.version_major == 1);
+    PingDeviceTest::test("Device Firmware",
+        device.device_information.firmware_version_major >= 3 && device.device_information.firmware_version_minor >= 27);
 
-    printf("set device id\n");
-    if (!device.set_device_id(1)) {
-        printf("failed to set device id\n");
-    } else {
-        printf("pass\n");
-    }
+    // Test commands
+    PingDeviceTest::test("Set mode", device.set_mode_auto(true));
+    PingDeviceTest::test("Set range", !device.set_range(100, 2000)); // We can't set range in auto mode
+    PingDeviceTest::test("Set mode", device.set_mode_auto(false));
+    PingDeviceTest::test("Set range", device.set_range(100, 2000)); // We are not in auto mode anymore
 
-    printf("set range\n");
-    if (!device.set_range(100, 30000)) {
-        printf("failed to set scan range\n");
-    } else {
-        printf("pass\n");
-    }
+    PingDeviceTest::test("Set speed of sound", device.set_speed_of_sound(1550000));
+    PingDeviceTest::test("Set mode", device.set_mode_auto(false));
+    PingDeviceTest::test("Set interval", device.set_ping_interval(200));
+    PingDeviceTest::test("Set gain", device.set_gain_setting(1));
+    PingDeviceTest::test("Set ping enable", device.set_ping_enable(1));
 
-    printf("set range\n");
-    if (!device.set_speed_of_sound(1550000)) {
-        printf("failed to set scan range\n");
-    } else {
-        printf("pass\n");
-    }
-
-    printf("set mode auto\n");
-    if (!device.set_mode_auto(false)) {
-        printf("failed to set device mode auto\n");
-    } else {
-        printf("pass\n");
-    }
-
-    printf("set ping interval\n");
-    if (!device.set_ping_interval(200)) {
-        printf("failed to set ping interval\n");
-    } else {
-        printf("pass\n");
-    }
-
-    printf("set gain setting\n");
-    if (!device.set_gain_setting(1)) {
-        printf("failed to set gain setting\n");
-    } else {
-        printf("pass\n");
-    }
-
-    printf("set ping enable\n");
-    if (!device.set_ping_enable(1)) {
-        printf("failed to set ping enable\n");
-    } else {
-        printf("pass\n");
-    }
-
-    auto testRequest = [&device](uint16_t msgId, const char* message = nullptr) {
-        printf("requesting message: %s\n",
-            message ? message : PingHelper::nameFromMessageId(static_cast<PingEnumNamespace::PingMessageId>(msgId)));
-        if (!device.request(msgId, 1000)) {
-            printf("fail\n");
-            return false;
-        }
-
-        printf("pass\n");
-        return true;
+    // Test request
+    auto requestIDs = {
+        Ping1dId::FIRMWARE_VERSION,
+        Ping1dId::DEVICE_ID,
+        Ping1dId::VOLTAGE_5,
+        Ping1dId::SPEED_OF_SOUND,
+        Ping1dId::RANGE,
+        Ping1dId::MODE_AUTO,
+        Ping1dId::PING_INTERVAL,
+        Ping1dId::GAIN_SETTING,
+        Ping1dId::TRANSMIT_DURATION,
+        Ping1dId::GENERAL_INFO,
+        Ping1dId::DISTANCE_SIMPLE,
+        Ping1dId::DISTANCE,
+        Ping1dId::PROCESSOR_TEMPERATURE,
+        Ping1dId::PCB_TEMPERATURE,
+        Ping1dId::PING_ENABLE,
+        Ping1dId::PROFILE,
     };
 
-    testRequest(Ping1dId::FIRMWARE_VERSION);
-    testRequest(Ping1dId::DEVICE_ID);
-    testRequest(Ping1dId::VOLTAGE_5);
-    testRequest(Ping1dId::SPEED_OF_SOUND);
-    testRequest(Ping1dId::RANGE);
-    testRequest(Ping1dId::MODE_AUTO);
-    testRequest(Ping1dId::PING_INTERVAL);
-    testRequest(Ping1dId::GAIN_SETTING);
-    testRequest(Ping1dId::TRANSMIT_DURATION);
-    testRequest(Ping1dId::GENERAL_INFO);
-    testRequest(Ping1dId::DISTANCE_SIMPLE);
-    testRequest(Ping1dId::DISTANCE);
-    testRequest(Ping1dId::PROCESSOR_TEMPERATURE);
-    testRequest(Ping1dId::PCB_TEMPERATURE);
-    testRequest(Ping1dId::PING_ENABLE);
-    testRequest(Ping1dId::PROFILE);
+    for (auto id : requestIDs) {
+        std::string title = "Requesting message [";
+        title.append(PingHelper::nameFromMessageId(static_cast<PingEnumNamespace::PingMessageId>(id)));
+        title.append("]");
+
+        PingDeviceTest::test(title, device.request(id, 1000));
+    }
 
     return 0;
 }
