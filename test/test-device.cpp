a3604f8c22368b38828dcb9b17df8f8e1469457d
Patrick José Pereira <patrickelectric@gmail.com>
2022-02-10 12:14:31 -0300

    test: test-device: Add support for ping360
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- test/test-device.cpp
+++ test/test-device.cpp
@@ -24,16 +24,31 @@ int main(int argc, char* argv[])
     // Basic information
     PingDeviceTest::test("Device initialization", device.initialize());
     PingDeviceTest::test("Device type", static_cast<int>(PingDeviceType::UNKNOWN) < device.device_information.device_type, Status::OK);
-    std::cout << "Device detected as: " << PingHelper::nameFromDeviceType(static_cast<PingDeviceType>(device.device_information.device_type)) << std::endl;
+    const auto device_type = static_cast<PingDeviceType>(device.device_information.device_type);
+    std::cout << "Device detected as: " << PingHelper::nameFromDeviceType(device_type) << std::endl;
     PingDeviceTest::test("Device ID", device.device_id != 0, Status::OK);
 
     // Common messages validation
     PingDeviceTest::test("Device Hardware revision", device.device_information.device_revision, Status::OK);
-    PingDeviceTest::test(
-        "Protocol version",
-        device.protocol_version.version_major == 1 && device.protocol_version.version_minor == 0 && device.protocol_version.version_patch == 0,
-        Status::OK
-    );
+    switch (device_type) {
+        case (PingDeviceType::PING1D):
+            PingDeviceTest::test(
+                "Protocol version",
+                device.protocol_version.version_major == 1 && device.protocol_version.version_minor == 0 && device.protocol_version.version_patch == 0,
+                Status::OK
+            );
+        break;
+        case (PingDeviceType::PING360):
+            PingDeviceTest::test(
+                "Protocol version",
+                device.protocol_version.version_major == 1 && device.protocol_version.version_minor == 1 && device.protocol_version.version_patch == 0,
+                Status::OK
+            );
+        break;
+        default:
+            printf("Protocol version: %d.%d.%d\n", device.protocol_version.version_major, device.protocol_version.version_minor, device.protocol_version.version_patch);
+
+    }
     PingDeviceTest::test(
         "Firmware version",
         device.device_information.firmware_version_major != 0 || device.device_information.firmware_version_minor != 0 || device.device_information.firmware_version_patch != 0,
